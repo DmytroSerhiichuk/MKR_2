c5dcb0a85b8404b5ee47540d201c5b40d23d6dab
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-05-31 10:50:38 +0300

    added gallery_view

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Category
+
+def gallery_view(request):
+    data = {
+        'categories' : Category.objects.all()
+    }
+    return render(request, 'gallery.html', context=data)
+
+def image_detail(request, pk):
+    ...
