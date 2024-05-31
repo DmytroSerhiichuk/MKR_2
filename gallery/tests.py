72865ede85405b280ac5339bc2d832613b4af2a8
DmytroSerhiichuk <157469257+DmytroSerhiichuk@users.noreply.github.com>
2024-05-31 11:12:12 +0300

    added test for gallery_view

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Image
 
 # Create your tests here.
+
+class GalleryViewTest(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name='Test Category 1')
+        Category.objects.create(name='Test Category 2')
+
+    def test_view_context_data(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(len(response.context['categories']), 2)
+        self.assertEqual(response.context['categories'][0].name, 'Test Category 1')
+        self.assertEqual(response.context['categories'][1].name, 'Test Category 2')
